60aad3f4b177eba396b058ccbbd27f095fab2775
yj2811 <yogakshijaiman@gmail.com>
2020-11-24 16:53:04 +0530

    Added largest

---
+++ OOP-Lab/Lab-1/Largest.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Largest{
+
+	static int largestOfThree(int n1, int n2, int n3){
+		int ans;
+
+		ans = (n1>n2)?((n1>n3)?n1:n3):((n2>n3)?n2:n3);
+
+		return ans;
+	}
+
+	public static void main(String args[]){
+		int n1, n2, n3, ans;
+
+		System.out.println("Enter the 3 numbers:");
+
+		Scanner sc = new Scanner(System.in);
+
+		n1 = sc.nextInt();
+		n2 = sc.nextInt();
+		n3 = sc.nextInt();
+
+		ans = largestOfThree(n1, n2, n3);
+
+		System.out.println("The largest number is:" + ans);
+
+	}
+
+ }
